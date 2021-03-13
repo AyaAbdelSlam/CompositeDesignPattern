a7cc07fd8e7d1f6ecafab0cad585a6824f11165c
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2021-03-13 22:21:40 +0200

    Initial Commit

---
+++ CompositeDesignPattern/IGiftOperation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositeDesignPattern
+{
+    public interface IGiftOperation
+    {
+        void Add(GiftBase gift);
+        void Remove(GiftBase gift);
+    }
+}
